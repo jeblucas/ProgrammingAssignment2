4847aa5166939dca41fade7212f49a4afa726317
unknown <jeba@USTO336450.am.corp.amgen.com>
2014-11-17 09:07:30 -0800

    Updated comments, first commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This couple of functions either 'solve's a matrix or it retrieves
+## a previously 'solve'd matrix from a local cache.
 
-## Write a short comment describing this function
+## This function turns a submitted matrix into a list that contains
+## a function that sets a value of a matrix, gets that value, sets
+## a value of the inverse of that matrix, and gets that.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function takes a matrix and 'solve's it, assuming the matrix
+## is not already 'solve'd and in the cache. If so, it just returns
+## the cached inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
